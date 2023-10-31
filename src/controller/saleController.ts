f02db6dff3c2b15a00b9ede68653f8004fb2cf9e
diendh03 <118904886+diendh03@users.noreply.github.com>
2023-10-31 10:47:48 +0700

    update

--- src/controller/saleController.ts
+++ src/controller/saleController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import Sale from "../model/sale";
 import * as cron from "node-cron";
 import Product from "../model/product";
-import { ISale } from "../interface/sale";
 export const createSale = async (req: Request, res: Response) => {
   try {
     const { discountPercentage, categoryId, startDate, endDate } = req.body;
@@ -32,7 +31,9 @@ export const createSale = async (req: Request, res: Response) => {
       });
       // console.log(products);
 
-      return res.json({ message: "Sale created and applied to products" });
+      return res.json({
+        message: "Cuộc giảm giá đã được tạo và áp dụng vào sản phẩm",
+      });
     } else {
       return res.json({
         message: "Cuộc giảm giá tại danh mục này đã được tạo",
@@ -40,7 +41,7 @@ export const createSale = async (req: Request, res: Response) => {
     }
   } catch (error) {
     console.error("Error creating sale:", error);
-    res.status(500).json({ error: "An error occurred" });
+    res.status(500).json({ error: "Có lỗi" });
   }
 };
 
@@ -72,9 +73,9 @@ const task = cron.schedule("0 0 * * *", async () => {
       });
     }
 
-    console.log("Sale prices updated for expired sales");
+    console.log("Giá giảm giá đã được cập nhật");
   } catch (error) {
-    console.error("Error updating sale prices:", error);
+    console.error("Lỗi:", error);
   }
 });
 
