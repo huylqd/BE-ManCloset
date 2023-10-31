685e22ce501f41c7a581d06e4baaa0147a8faa62
diendh03 <118904886+diendh03@users.noreply.github.com>
2023-10-31 10:39:27 +0700

    Tạo cuộc giảm giá và update tự động

--- src/interface/product.ts
+++ src/interface/product.ts
@@ -1,17 +1,18 @@
 import mongoose, { Document } from "mongoose";
-interface property {
+
+type property = {
   imageUrl: string;
   color: string;
   quantity: number;
   size: string;
-}
+};
 export interface IProduct extends Document {
   productName: string;
   price: number;
   description?: string;
   properties: property[];
   categoryId: mongoose.Types.ObjectId;
-  couponId: mongoose.Types.ObjectId;
+  discount: number;
   createdAt?: Date;
   updatedAt?: Date;
   deletedAt?: Date | null;
