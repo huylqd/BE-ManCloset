3671242520537aecfe5f9426063979f0db0a8559
QuangSon0409 <buiquangson7782@gmail.com>
2023-11-02 09:08:31 +0700

    update router cate

--- src/routers/category.ts
+++ src/routers/category.ts
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get("/category", getAllCategory);
 router.get("/category/:id", getCategoryById);
-router.put("/category/:id", updateCategory);
+router.patch("/category/:id", updateCategory);
 router.delete("/category/:id", removeCategory);
 router.post("/category", createCategory);
 
