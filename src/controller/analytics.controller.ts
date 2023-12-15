de21a60036f5642edba105d0eb6dbf9ec19c2bf7
diendh03 <118904886+diendh03@users.noreply.github.com>
2023-12-16 05:10:58 +0700

    analyst

--- src/controller/analytics.controller.ts
+++ src/controller/analytics.controller.ts
@@ -2,20 +2,54 @@ import Bill from "../model/order";
 import { Request, Response } from "express";
 import User from "../model/user";
 
-export const productSold = async (req: Request, res: Response) => {
-  // Lấy ngày bắt đầu và kết thúc của tháng hiện tại
-  const startDate = new Date(
-    new Date().getFullYear(),
-    new Date().getMonth(),
-    1
-  );
-  const endDate = new Date(
-    new Date().getFullYear(),
-    new Date().getMonth() + 1,
-    0
-  );
+// Lấy ngày bắt đầu và kết thúc của tháng hiện tại
+const startOfMonth = new Date(
+  new Date().getFullYear(),
+  new Date().getMonth(),
+  1
+);
+const endOfMonth = new Date(
+  new Date().getFullYear(),
+  new Date().getMonth() + 1,
+  0
+);
+// Lấy ngày bắt đầu và kết thúc của ngày hiện tại
+const startOfDay = new Date();
+startOfDay.setHours(0, 0, 0, 0);
 
+const endOfDay = new Date();
+endOfDay.setHours(23, 59, 59, 999);
+// Lấy ngày bắt đầu và kết thúc của năm hiện tại
+const startOfYear = new Date(new Date().getFullYear(), 0, 1);
+const endOfYear = new Date(new Date().getFullYear(), 11, 31, 23, 59, 59, 999);
+// Lấy ngày bắt đầu và kết thúc của tuần hiện tại
+const currentDate = new Date();
+const startOfWeek = new Date(
+  currentDate.setDate(currentDate.getDate() - currentDate.getDay())
+); // Ngày đầu tuần (chủ nhật)
+const endOfWeek = new Date(startOfWeek);
+endOfWeek.setDate(endOfWeek.getDate() + 6); // Ngày cuối tuần (thứ bảy)
+
+export const productSold = async (req: Request, res: Response) => {
   try {
+    const { filter } = req.body;
+    let startDate, endDate;
+    if (filter === "Ngày") {
+      startDate = startOfDay;
+      endDate = endOfDay;
+    }
+    if (filter === "Tuần") {
+      startDate = startOfWeek;
+      endDate = endOfWeek;
+    }
+    if (filter === "Tháng" || !filter) {
+      startDate = startOfMonth;
+      endDate = endOfMonth;
+    }
+    if (filter === "Năm") {
+      startDate = startOfYear;
+      endDate = endOfYear;
+    }
     //tìm sản phẩm bán chạy nhất
     const result = await Bill.aggregate([
       {
@@ -60,28 +94,30 @@ export const productSold = async (req: Request, res: Response) => {
 };
 export const Thongkedoanhso = async (req: Request, res: Response) => {
   try {
-    const currentDate = new Date();
-    const startOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      1
-    );
-    const endOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth() + 1,
-      0,
-      23,
-      59,
-      59,
-      999
-    );
-
+    const { filter } = req.body;
+    let startDate, endDate;
+    if (filter === "Ngày") {
+      startDate = startOfDay;
+      endDate = endOfDay;
+    }
+    if (filter === "Tuần") {
+      startDate = startOfWeek;
+      endDate = endOfWeek;
+    }
+    if (filter === "Tháng" || !filter) {
+      startDate = startOfMonth;
+      endDate = endOfMonth;
+    }
+    if (filter === "Năm") {
+      startDate = startOfYear;
+      endDate = endOfYear;
+    }
     const result = await Bill.aggregate([
       {
         $match: {
           createdAt: {
-            $gte: startOfMonth,
-            $lte: endOfMonth,
+            $gte: startDate,
+            $lte: endDate,
           },
         },
       },
@@ -110,28 +146,30 @@ export const Thongkedoanhso = async (req: Request, res: Response) => {
 };
 export const Thongketaikhoan = async (req: Request, res: Response) => {
   try {
-    const currentDate = new Date();
-    const startOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      1
-    );
-    const endOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth() + 1,
-      0,
-      23,
-      59,
-      59,
-      999
-    );
-
+    const { filter } = req.body;
+    let startDate, endDate;
+    if (filter === "Ngày") {
+      startDate = startOfDay;
+      endDate = endOfDay;
+    }
+    if (filter === "Tuần") {
+      startDate = startOfWeek;
+      endDate = endOfWeek;
+    }
+    if (filter === "Tháng" || !filter) {
+      startDate = startOfMonth;
+      endDate = endOfMonth;
+    }
+    if (filter === "Năm") {
+      startDate = startOfYear;
+      endDate = endOfYear;
+    }
     const result = await User.aggregate([
       {
         $match: {
           createdAt: {
-            $gte: startOfMonth,
-            $lte: endOfMonth,
+            $gte: startDate,
+            $lte: endDate,
           },
         },
       },
@@ -161,28 +199,30 @@ export const Thongketaikhoan = async (req: Request, res: Response) => {
 
 export const Thongkedonhang = async (req: Request, res: Response) => {
   try {
-    const currentDate = new Date();
-    const startOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      1
-    );
-    const endOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth() + 1,
-      0,
-      23,
-      59,
-      59,
-      999
-    );
-
+    const { filter } = req.body;
+    let startDate, endDate;
+    if (filter === "Ngày") {
+      startDate = startOfDay;
+      endDate = endOfDay;
+    }
+    if (filter === "Tuần") {
+      startDate = startOfWeek;
+      endDate = endOfWeek;
+    }
+    if (filter === "Tháng" || !filter) {
+      startDate = startOfMonth;
+      endDate = endOfMonth;
+    }
+    if (filter === "Năm") {
+      startDate = startOfYear;
+      endDate = endOfYear;
+    }
     const result = await Bill.aggregate([
       {
         $match: {
           createdAt: {
-            $gte: startOfMonth,
-            $lte: endOfMonth,
+            $gte: startDate,
+            $lte: endDate,
           },
         },
       },
@@ -207,5 +247,78 @@ export const Thongkedonhang = async (req: Request, res: Response) => {
     return res.status(500).json({
       message: error.message,
     });
+  }
+};
+export const topUserMuaHang = async (req: Request, res: Response) => {
+  try {
+    const { filter } = req.body;
+    let startDate, endDate;
+    if (filter === "Ngày") {
+      startDate = startOfDay;
+      endDate = endOfDay;
+    }
+    if (filter === "Tuần") {
+      startDate = startOfWeek;
+      endDate = endOfWeek;
+    }
+    if (filter === "Tháng" || !filter) {
+      startDate = startOfMonth;
+      endDate = endOfMonth;
+    }
+    if (filter === "Năm") {
+      startDate = startOfYear;
+      endDate = endOfYear;
+    }
+    //tìm sản phẩm bán chạy nhất
+    const result = await Bill.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: startDate,
+            $lte: endDate,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$user_id", // Nhóm theo user_id
+          totalSpent: { $sum: "$total_price" }, // Tính tổng số tiền đã chi tiêu
+        },
+      },
+      {
+        $sort: {
+          totalSpent: -1, // Sắp xếp theo tổng số tiền giảm dần
+        },
+      },
+      {
+        $limit: 5,
+      },
+      {
+        $lookup: {
+          from: "users", // Tên của bảng Users
+          localField: "_id", // Trường cần so sánh trong bảng Order
+          foreignField: "_id", // Trường cần so sánh trong bảng Users
+          as: "userDetails", // Tên của trường chứa thông tin người dùng
+        },
+      },
+      {
+        $unwind: "$userDetails", // Mở rộng mảng 'userDetails' thành các bản ghi riêng lẻ
+      },
+      {
+        $project: {
+          _id: "$userDetails._id",
+          name: "$userDetails.name",
+          email: "$userDetails.email",
+          totalSpent: 1,
+        },
+      },
+    ]).exec();
+
+    return res.status(200).json({
+      message: "Top 5 khách hàng chi tiêu nhiều",
+      data: result,
+    });
+  } catch (error) {
+    return error.message;
   }
 };
